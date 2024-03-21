0e330bcaed4a8eb2a7ad0aafae627cb8bc675526
victorgustavodev <victorgustavo.dev@gmail.com>
2024-03-21 17:50:52 -0300

    add features

---
+++ assets/script/reveal.js
@@ -0,0 +1,39 @@
+// window.revelar = ScrollReveal({ reset: true });
+
+//hero
+revelar.reveal(".hero-reveal", {
+  duration: 3000,
+  distance: "5rem",
+});
+
+revelar.reveal(".features-reveal", {
+  duration: 2000,
+  distance: "3rem",
+});
+
+revelar.reveal(".content-1-reveal", {
+  duration: 2000,
+  distance: "3rem",
+  origin: "left",
+});
+
+revelar.reveal(".facts-reveal", {
+  duration: 3500,
+  distance: "0",
+});
+
+revelar.reveal(".content-2-reveal", {
+  duration: 4500,
+  distance: "3rem",
+  origin: "left",
+});
+
+revelar.reveal(".assessment .box-1", {
+  duration: 2500,
+  distance: "6rem",
+});
+
+revelar.reveal(".assessment .box-2", {
+    duration: 2500,
+    distance: "6rem",
+  });
---
+++ assets/script/script.js
@@ -0,0 +1,7 @@
+// Função para rolar suavemente até o topo da página
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Rola suavemente
+  });
+}
