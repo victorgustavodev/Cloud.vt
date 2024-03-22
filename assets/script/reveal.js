91eca4aac14a4018bb0b037bfedcccbfbef6147a
victorgustavodev <victorgustavo.dev@gmail.com>
2024-03-21 22:09:00 -0300

    firstUpdate

--- assets/script/reveal.js
+++ assets/script/reveal.js
@@ -1,4 +1,10 @@
-// window.revelar = ScrollReveal({ reset: true });
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal(".header-reveal", {
+  duration: 1000,
+  distance: '3rem',
+  origin:'top'
+});
 
 //hero
 revelar.reveal(".hero-reveal", {
@@ -18,7 +24,7 @@ revelar.reveal(".content-1-reveal", {
 });
 
 revelar.reveal(".facts-reveal", {
-  duration: 3500,
+  duration: 3000,
   distance: "0",
 });
 
@@ -37,3 +43,38 @@ revelar.reveal(".assessment .box-2", {
     duration: 2500,
     distance: "6rem",
   });
+  
+  revelar.reveal(".plan h2", {
+    duration: 1000,
+    distance: "2rem",
+  });
+
+  revelar.reveal(".plan h4", {
+    duration: 1500,
+    distance: "2rem"
+  });
+
+revelar.reveal(".card-1", {
+  duration: 2000,
+  distance: "6rem", 
+  opacity: '0.2'
+});
+
+revelar.reveal(".card-2", {
+  duration: 3000,
+  distance: "6rem", 
+  opacity: '0.2',
+});
+
+revelar.reveal(".card-3", {
+  duration: 4000,
+  distance: "6rem", 
+  opacity: '0.2',
+});
+
+
+revelar.reveal(".grid-plan-reveal", {
+  duration: 4000,
+  distance: '3rem',
+  opacity: '0.2',
+});
